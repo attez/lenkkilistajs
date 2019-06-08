eb3d8c72a604e45fe502a6b543ef197638affe8d
Atte Sairanen <atte.sairanen@gmail.com>
2019-06-08 16:44:30 +0300

    Added component that is used to show properties for a workout

--- src/components/WorkoutInfo.vue
+++ src/components/WorkoutInfo.vue
@@ -2,10 +2,12 @@
     <v-card>
         <v-card-text> {{ workout.start_time | date }} </v-card-text>
         <v-card-title primary-title>
-            <router-link :to="{name: ''}"> <h2> {{ workout.name }} </h2> </router-link>
+            <router-link :to="{name: 'workout', params: {workout_id: workout.id } }"> <h2> {{ workout.name }} </h2> </router-link>
         </v-card-title>
         <v-card-text>
             <p>{{ workout.description }}</p>
+            <WorkoutProperty name="ominaisuus" unit="km">124.20</WorkoutProperty>
+
         </v-card-text>
         
         
@@ -13,6 +15,7 @@
 </template>
 
 <script>
+import WorkoutProperty from '@/components/WorkoutProperty'
 export default {
     name: 'WorkoutInfo',
     data() {
@@ -23,6 +26,9 @@ export default {
     },
     props: {
         workout: null
+    },
+    components: {
+        WorkoutProperty
     }
     /*
     HAHMOTELMA
---
+++ src/components/WorkoutProperty.vue
@@ -0,0 +1,34 @@
+<template>
+    <v-sheet class="pa-1" color="red lighten-4" width=140>
+        <v-layout column align-center>
+            <v-flex>
+                <v-flex class="caption font-weight-light">
+                    {{ name }}
+                </v-flex>
+                <v-flex class="title">
+                    <slot></slot> {{ unit }}                      
+                </v-flex>
+            </v-flex>
+        </v-layout>
+    </v-sheet>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+    props: {
+        name: String,
+        unit: String
+    }
+    
+}
+</script>
+
+<style>
+
+</style>
+
