ef8cb14d9a2e812cab43bada2923d3d624cfa7ce
Atte Sairanen <atte.sairanen@gmail.com>
2019-06-12 20:54:50 +0300

    Hide signin and signup when logged in.

--- src/EventBus.js
+++ src/EventBus.js
@@ -1,5 +1,12 @@
 import Vue from 'vue'
 const EventBus = new Vue()
 export default EventBus
+/*
+Event bus is used to notify components of changes in other (sibling) components.
+Events:
+    userChanged: when user status changes (login, logout)
 
-// instead of bus could just use firebase.auth().onAuthStateChanged on components' created() hooks
+
+Instead of bus could just use firebase.auth().onAuthStateChanged on components' created() hooks. Or vuex.
+
+*/
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,8 +2,8 @@
 <v-container>
   <v-layout column>
     <v-flex xs-12> <AppDescription/> </v-flex>
-    <v-flex xs-12> <SignUp/> </v-flex>
-    <v-flex xs-12> <SignIn/> </v-flex>
+    <v-flex v-if="!isAuthtenticated" xs-12> <SignUp/> </v-flex>
+    <v-flex v-if="!isAuthtenticated" xs-12> <SignIn/> </v-flex>
     
   </v-layout>
 </v-container>
@@ -13,12 +13,28 @@
   import AppDescription from '../components/AppDescription'
   import SignUp from '../components/SignUp'
   import SignIn from '../components/SignIn'
+  import bus from '@/EventBus'
+  import * as firebase from 'firebase/app'
+  import 'firebase/auth'
+  
 
   export default {
+    name: 'Home',
     components: {
       AppDescription,
       SignUp,
       SignIn
+    },
+    data() {
+      return {
+        isAuthtenticated: !!firebase.auth().currentUser
+
+      }
+    },
+    created(){
+        bus.$on('userChanged', user => { 
+            this.isAuthtenticated = !!user
+        })
     }
   }
 </script>
